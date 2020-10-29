ded1af46a7d3457634c08c27a034c5f129af92bf
shewood <shewood01@gmail.com>
2020-10-29 11:44:59 -0400

    try and get strapi running

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'benefitsfinder'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', ''),
+        port: env.int('DATABASE_PORT', 5433),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
