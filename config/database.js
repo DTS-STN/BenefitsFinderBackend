ba0f0dffa0d7e6ea7ab84416c837976bdacec071
Moro-Code <omar@omarnasr.ca>
2020-10-29 12:18:42 -0400

    works with pg container

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '0.0.0.0'),
         port: env.int('DATABASE_PORT', 5433),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
